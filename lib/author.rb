3bb16f691d6fb4a7668ebf09dfed247bcb967b5a
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-01 20:48:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,7 @@ class Author
     @@post_count += 1
   end
 
-  def add_post_by_title(name)
+  def add_post_by_title(title)
     new_post = Post.new(title)
     new_post.author = self
     @posts << new_post
