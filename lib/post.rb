df8baf6a119f21dc7e128f1032e38dd07c690498
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-01 20:42:31 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,24 @@
 class Post
   attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    if self.author.nil?
+      nil
+    else
+      self.author.name
+    end
+  end
+  
 end
