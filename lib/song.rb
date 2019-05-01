6e9a0b38ae681c8b319cfa8baa3bcb8d0721d2db
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-01 20:45:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,22 @@
 class Song
   attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+
+  def self.all
+    @all
+  end  
+
+  def song_name
+    if self.song.nil?
+      nil
+    else
+      self.song.name
+    end
+  end
 end
